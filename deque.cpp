d2a5afa98fee82eefd8298cd0397f610065ec7a1
stanleya1293 <astanle8@ut.utm.edu>
2023-12-02 15:18:32 -0600

    corrected deque

---
+++ deque.cpp
@@ -0,0 +1,45 @@
+#include "deque.h"
+
+Deque::Deque() {
+  blockmap = new int* [1];
+}
+
+Deque::~Deque() {
+
+}
+
+void Deque::push_front(int value) {
+  
+}
+
+void Deque::pop_front(int value) {
+
+}
+
+void Deque::push_back(int value) {
+
+}
+
+void Deque::pop_back(int value) {
+
+}
+
+int Deque::front() {
+
+}
+
+int Deque::back() {
+
+}
+
+bool Deque::empty() {
+
+}
+
+int Deque::size() {
+
+}
+
+int Deque::operator[](int index) {
+  
+}
